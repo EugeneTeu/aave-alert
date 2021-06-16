9c1cc3170b429e67d7183593a717c38c95996768
eugene teu <eugeneteu@gmail.com>
2021-06-16 21:59:32 +0800

    figuring out wtf is graphql

--- aave-functions/index.js
+++ aave-functions/index.js
@@ -1,93 +1,75 @@
-import { ApolloServer, gql } from "apollo-server"
-
+import { createRequire } from 'module';
+import { ApolloServer, gql, makeExecutableSchema, addSchemaLevelResolveFunction, makeRemoteExecutableSchema, introspectSchema } from "apollo-server"
+import { HttpLink } from "apollo-link-http"
+import  fetch  from "node-fetch"
 import { v1, v2 } from '@aave/protocol-js';
-
 import { loadSchema} from "@graphql-tools/load"
 import { UrlLoader } from "@graphql-tools/url-loader"
 import { addResolversToSchema } from "@graphql-tools/schema"
 import express from 'express';
 import { graphqlHTTP }  from 'express-graphql';
+import { request, GraphQLClient } from 'graphql-request'
+
+console.log("YOUR_ADDRESS_HERE".toLowerCase())
+const aaveMaticSubgraphEndpoint = 'https://api.thegraph.com/subgraphs/name/aave/aave-v2-matic'
+
+// create a GraphQL client instance to send requests
+const client = new GraphQLClient(aaveMaticSubgraphEndpoint, { headers: {} })
+
+const testQuery = gql`
+  {
+  reserves (where: {
+    usageAsCollateralEnabled: true
+  }) {
+    id
+    name
+    price {
+      id
+    }
+    liquidityRate
+    variableBorrowRate
+    stableBorrowRate
+  }
+}
+`
+
+const executeQuery = async (client, query) => {
+   const response = await client.request(query)
+   console.log(response)
+}
+// we can simply use this client to run quries.
+executeQuery(client, testQuery)
 
 
 
-// matic subgraph
-const schema = await loadSchema('https://api.thegraph.com/subgraphs/name/aave/aave-v2-matic', {   // load from endpoint
-    loaders: [
-        new UrlLoader()
-    ]
-});
+// -----------------------------------------------------------------------------
+// Graph ql express server
+// -----------------------------------------------------------------------------
 
-// Write some resolvers
-const resolvers = {};
+// const schema1 = await loadSchema('https://api.thegraph.com/subgraphs/name/aave/aave-v2-matic', {   // load from endpoint
+//     loaders: [
+//         new UrlLoader()
+//     ]
+// });
 
-// Add resolvers to the schema
-const schemaWithResolvers = addResolversToSchema({
-    schema,
-    resolvers,
-});
+// // Write some resolvers
+// const resolvers = {};
 
-const app = express();
-
-app.use(
-    graphqlHTTP({
-        schema: schemaWithResolvers,
-        graphiql: true,
-    })
-);
-
-app.listen(4000, () => {
-    console.info(`Server listening on http://localhost:4000`)
-})
-
-
-
-
-
-// // A schema is a collection of type definitions (hence "typeDefs")
-// // that together define the "shape" of queries that are executed against
-// // your data.
-// const typeDefs = gql`
-
-
-//   type UserReserveData {
-//     reserve {
-//       symbol
-//     }
-//     currentTotalDebt
-//     currentVariableDebt
-//   }
-
-//   # The "Query" type is special: it lists all of the available queries that
-//   # clients can execute, along with the return type for each. In this
-//   # case, the "books" query returns an array of zero or more Books (defined above).
-//   type Query {
-//     getUserReserve : UserReserveData
-//   }
-  
-  
-
-
-// `;
-
-// // Resolvers define the technique for fetching the types defined in the
-// // schema. This resolver retrieves books from the "books" array above.
-// const resolvers = {
-//   Query: {
-//     getUserReserve : () => 
-//   },
-// };
-
+// // Add resolvers to the schema
 // const schemaWithResolvers = addResolversToSchema({
 //     schema,
 //     resolvers,
 // });
 
+// // const app = express();
 
-// // The ApolloServer constructor requires two parameters: your schema
-// // definition and your set of resolvers.
-// const server = new ApolloServer({ typeDefs, schemaWithResolvers });
+// // app.use(
+// //     graphqlHTTP({
+// //         schema: schemaWithResolvers,
+// //         graphiql: true,
+// //     })
+// // );
 
-// // The `listen` method launches a web server.
-// server.listen().then(({ url }) => {
-//   console.log(`🚀  Server ready at ${url}`);
-// });
+// // app.listen(4000, () => {
+// //     console.info(`Server listening on http://localhost:4000`)
+// // })
